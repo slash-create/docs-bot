29a9c541b8eb9dc4c3bea6c055982a38e3be71c5
Junior <sudojunior@outlook.com>
2022-07-18 23:45:15 +0100

    feat: `/code` command (iteration 1) * command is able to fetch a file from a doc entry meta and slice {x} lines currently set at `50` * if either {endLine} or {startLine} go out of bounds in terms of line numbers, they are brought back in range - in that order with {startLine} being offset further up the file if any content is there
    ! iteration 2 will have this change into a command set with the current structure being set to `/code entry` with the same autocompletion, plus an option to 'wrap around' the targeted doc entry
    ! the second command will be `/code lines file: string from: int to: int` - `file` will be autocompleted with a change made to `TypeNavigator` to gather all unique file paths upon starting, `from` and `to` will act very much like themselves but line range hints may be given in autocompletion for `file`

---
+++ src/commands/code.ts
@@ -0,0 +1,111 @@
+import bent from 'bent';
+
+const getText = bent('string');
+
+import {
+  AutocompleteChoice,
+  AutocompleteContext,
+  ButtonStyle,
+  CommandContext,
+  CommandOptionType,
+  ComponentType,
+  MessageOptions,
+  SlashCommand,
+  SlashCreator
+} from 'slash-create';
+import { buildGitHubLink, rawContentLink } from '../util/linkBuilder';
+import TypeNavigator from '../util/typeNavigator';
+
+export default class CodeCommand extends SlashCommand {
+  offset = 50;
+
+  constructor(creator: SlashCreator) {
+    super(creator, {
+      name: 'code',
+      description: 'Get a section of code from the source repository.',
+      options: [
+        {
+          name: 'query',
+          description: 'The query to search all entries.',
+          type: CommandOptionType.STRING,
+          autocomplete: true,
+          required: true
+        }
+      ]
+    });
+  }
+
+  autocomplete = async ({ options }: AutocompleteContext): Promise<AutocompleteChoice[]> =>
+    TypeNavigator.fuzzyFilter(options.query as string).map<AutocompleteChoice>((entry) => ({
+      name: `${entry.string} {score: ${entry.score}}`,
+      value: entry.string
+    }));
+
+  async run(ctx: CommandContext): Promise<MessageOptions | void | string> {
+    const { query } = ctx.options;
+
+    if (!(query in TypeNavigator.typeMap.all)) {
+      return {
+        content: 'Symbol not found',
+        ephemeral: true
+      };
+    }
+
+    const { meta } = TypeNavigator.findFirstMatch(query);
+    const file = `${meta.path}/${meta.file}`;
+    const res: string = await getText(`${rawContentLink}/${file}`);
+
+    const lines = res.split('\n');
+    let startLine = meta.line - 1; // slice offset
+    let endLine = meta.line + this.offset;
+
+    if (endLine > lines.length) {
+      startLine -= endLine - this.offset;
+      endLine = lines.length;
+    }
+
+    if (startLine <= 0) startLine = 0;
+
+    const lineOffset = meta.line + this.offset;
+    const lineSelection = lines.slice(startLine, endLine);
+
+    let content = [
+      `\`${file}\` - Lines \`${startLine + 1}\` to \`${endLine}\``,
+      '```js',
+      lineSelection
+        .map((line, index) => `/* ${`${startLine + index + 1}`.padStart(`${endLine}`.length, ' ')} */ ${line}`)
+        .join('\n'),
+      '```'
+    ].join('\n');
+
+    let actualEnd = lineOffset;
+
+    while (content.length > 2000) {
+      const lines = content.split('\n');
+      lines.splice(-2, 1);
+      actualEnd--;
+      lines[0] = `\`${file}\` - Lines \`${startLine + 1}\` to ~~\`${endLine}\`~~ \`${actualEnd}\``;
+      content = lines.join('\n');
+    }
+
+    ctx.send({
+      content,
+      components: [
+        {
+          type: ComponentType.ACTION_ROW,
+          components: [
+            {
+              type: ComponentType.BUTTON,
+              style: ButtonStyle.LINK,
+              url: buildGitHubLink(meta),
+              label: 'Open GitHub',
+              emoji: {
+                name: '📂'
+              }
+            }
+          ]
+        }
+      ]
+    });
+  }
+}
--- src/util/linkBuilder.ts
+++ src/util/linkBuilder.ts
@@ -2,12 +2,14 @@ import { FileMeta } from './metaTypes';
 
 export const BRANCH = `master`; // possiblity to add branch support further in, but i wouldn't see much point if discord keeps changing stuff...
 export const BASE_DOCS_URL = `https://slash-create.js.org/#/docs/main/${BRANCH}`;
-export const BASE_GITHUB_URL = `https://github.com/Snazzah/slash-create/blob/${BRANCH}`;
+export const REMOTE_LOCATION = `Snazzah/slash-create`;
 
 export function buildDocsLink(calledType: string, entity: string, scrollTo?: string) {
   return `${BASE_DOCS_URL}/${calledType}/${entity}${scrollTo ? `?scrollTo=${scrollTo}` : ''}`;
 }
 
 export function buildGitHubLink(fileMeta: FileMeta) {
-  return `${BASE_GITHUB_URL}/${fileMeta.path}/${fileMeta.file}#L${fileMeta.line}`;
+  return `https://github.com/${REMOTE_LOCATION}/blob/${BRANCH}/${fileMeta.path}/${fileMeta.file}#L${fileMeta.line}`;
 }
+
+export const rawContentLink = `https://raw.githubusercontent.com/${REMOTE_LOCATION}/${BRANCH}`;
