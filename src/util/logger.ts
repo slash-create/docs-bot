2d3033cd708e27bee7961d26e8a06121cb06da68
Junior <sudojunior@outlook.com>
2023-03-09 01:10:09 +0000

    patch: remove unnecessary call for debug logging in deployment

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -3,5 +3,4 @@ import CatLoggr from 'cat-loggr/ts';
 //
 export const logger = new CatLoggr()
   .setLevel(process.env.COMMANDS_DEBUG === 'true' ? 'debug' : 'info')
-  .setLevel('debug')
   .setGlobal();
