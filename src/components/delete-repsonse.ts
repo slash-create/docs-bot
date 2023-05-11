16d61af93a38276a8298195697a2318ae4eaeb25
Junior <sudojunior@outlook.com>
2023-05-11 18:55:53 +0100

    initial attempts to add

---
+++ src/components/delete-repsonse.ts
@@ -0,0 +1,29 @@
+import { ButtonStyle, CommandContext, ComponentButton, ComponentContext, ComponentType, SlashCreator } from 'slash-create';
+import { ComponentKeys } from '.';
+
+export const component: ComponentButton = {
+  type: ComponentType.BUTTON,
+  custom_id: ComponentKeys.DELETE_RESPONSE,
+  style: ButtonStyle.DESTRUCTIVE,
+  label: '', // not needed?
+  emoji: {
+    name: '🗑'
+  }
+}
+
+export function hasPermission(context: CommandContext | ComponentContext): boolean {
+  if (context.channel.type === 1) return true; // DM
+  // GROUP_DM - context.channel.type === 3 && context.channel.owner_id === context.user.id
+  if ('targetMessage' in context) {
+    if (context.targetMessage.author.id === context.user.id) return true; // AUTHOR
+  }
+
+  return context.member?.permissions.any(['ADMINISTRATOR', 'MANAGE_MESSAGES']) ?? false; // GUILD
+}
+
+export async function deleteResponse(creator: SlashCreator, context: ComponentContext) {
+  if (!hasPermission(context)) return false;
+
+  await context.acknowledge();
+  await context.delete(context.message.id);
+}
---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import { SlashCreator } from "slash-create";
+import { deleteResponse } from "./delete-repsonse";
+
+export enum ComponentKeys {
+  DELETE_RESPONSE = "delete-response"
+}
+
+export default function registerComponents(creator: SlashCreator) {
+
+}
