fcffb4205b2b5857b42bef1a9cddc669a6adc8ce
Junior <sudojunior@outlook.com>
2023-11-10 12:38:40 +0000

    patch 1ce29006a6, author is expected to be self

--- src/commands/debug/message.ts
+++ src/commands/debug/message.ts
@@ -29,7 +29,7 @@ export default class MessageDebugCommand extends SlashCommand {
     const origin = 'guild_id' in ctx.data ? ctx.data.guild_id : '@me';
     const target_url = `https://discord.com/channels/${origin}/${ctx.data.channel_id}/${target_id}`;
 
-    if (ctx.targetMessage.author.id !== this.creator.options.applicationID)
+    if (ctx.targetMessage.author.id === this.creator.options.applicationID)
       this.tryDeferredAdjustment(ctx);
 
     return ChatDebugCommand.resolveFinalPayload(rawPayload, 'message', target_url);
